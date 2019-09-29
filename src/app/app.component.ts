9bf194dd6cbfdaa977a257fb4bb8dd3ad1beb1ac
jsonamit <jsonamit@gmail.com>
2019-09-30 00:32:59 +0530

    dynamic reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder,FormGroup,Validators, FormArray, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dyn-reactive';
+  form :FormGroup;
+  constructor(private fb:FormBuilder) {
+   
+  }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      // name : ['',Validators.required],
+      products : this.fb.array([
+        this.createProducts()
+      ]) 
+    });
+  }
+
+  createProducts():FormGroup{
+    return this.fb.group({
+     name : '',
+     description : '',
+    });
+  }
+  // 
+  addProducts() {
+    const product = this.createProducts();
+    this.products.push(product);
+
+    //(<FormArray>this.form.get('products')).push(new FormControl(''));
+  }
+
+  removeProducts(index:any) {
+    (<FormArray>this.form.get('products')).removeAt(index);
+  }
+
+  get products():FormArray {
+   return this.form.get('products') as FormArray;
+  }
+
 }
